600be38c6ecbe8f3d53f04ba1b8e02f375a524e6
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2022-04-25 17:45:06 +0530

    completed extraction

--- TF_IDF.py
+++ TF_IDF.py
@@ -11,6 +11,7 @@ class TF_IDF_Vectorizer:
 		self.use_cached = use_cached
 		self.input_dir = input_dir
 		self.output_dir = output_dir
+		self.file_array = os.listdir(self.input_dir)
 		if not os.path.exists(output_dir):
 			os.mkdir(output_dir)
 		if stemmer is None:
@@ -19,14 +20,14 @@ class TF_IDF_Vectorizer:
 		if vocab is None:
 			vocab = self.get_vocab()
 		self.vocab = vocab
-		self.tf = self.get_term_frequency()
 		self.idf = self.get_inverse_document_frequency()
+		self.tf = self.get_term_frequency()
 		self.similarities = []
 
 	def get_vocab(self):
 		if not os.path.exists(self.output_dir+"vocab.dat") or not self.use_cached:
 			vocab = []
-			bar = tqdm([self.input_dir+i for i in os.listdir(self.input_dir)])
+			bar = tqdm([self.input_dir+i for i in self.file_array])
 			for file in bar:
 				with open(file, "r", encoding="utf-8") as f:
 					data = f.read()
@@ -57,7 +58,7 @@ class TF_IDF_Vectorizer:
 		for word,count in zip(words, counts):
 			try:
 				documents["columns"].append(self.vocab.index(word))
-				documents["value"].append(count)
+				documents["value"].append(count*self.idf[word])
 				counter+=1
 			except:
 				continue
@@ -65,8 +66,8 @@ class TF_IDF_Vectorizer:
 
 	def get_term_frequency(self):
 		if not os.path.exists(self.output_dir+"tf.dat") or not self.use_cached:
-			documents = {"value": [], "columns": [], "rows": [0]}
-			bar = tqdm([self.input_dir+i for i in os.listdir(self.input_dir)])
+			documents = {"value": [], "columns": [], "rows": [0], "files": []}
+			bar = tqdm([self.input_dir+i for i in self.file_array], desc="extracting TF")
 			for file in bar:
 				with open(file, "r", encoding="utf-8") as f:
 					data = f.read()
@@ -74,6 +75,7 @@ class TF_IDF_Vectorizer:
 				documents["value"].extend(document["value"])
 				documents["columns"].extend(document["columns"])
 				documents["rows"].append(counter+documents["rows"][-1])
+				documents["files"].append(file)
 			with open(self.output_dir+"tf.dat", "wb") as f:
 				pickle.dump(documents, f)
 		else:
@@ -85,7 +87,7 @@ class TF_IDF_Vectorizer:
 	def get_inverse_document_frequency(self):
 		if not os.path.exists(self.output_dir+"idf.dat") or not self.use_cached:
 			df = {word:0 for word in self.vocab}
-			bar = tqdm([self.input_dir+i for i in os.listdir(self.input_dir)])
+			bar = tqdm([self.input_dir+i for i in self.file_array], desc="extracting IDF")
 			for file in bar:
 				with open(file, "r", encoding="utf-8") as f:
 					data = f.read()
@@ -98,8 +100,8 @@ class TF_IDF_Vectorizer:
 							df[word] += 1
 						except:
 							pass
-			N = len(os.listdir(self.input_dir))
-			idf = {key:np.log2(N/df[key]) for key in tqdm(df.keys())}
+			N = len(self.file_array)
+			idf = {key:np.log2(N/df[key]) for key in tqdm(df.keys(), disable=True)}
 			with open(self.output_dir+"idf.dat", "wb") as f:
 				pickle.dump(idf, f)
 		else:
@@ -119,11 +121,15 @@ class TF_IDF_Vectorizer:
 			for val,col in zip(values, columns):
 				dot_product += val*document_vec[col]
 			values = np.array(values)
-			cosine = dot_product/(np.sqrt(np.sum(document_vec**2))*np.sqrt(np.sum(values**2)))
+			denom = np.sqrt(np.sum(document_vec**2))*np.sqrt(np.sum(values**2))
+			if denom!=0:
+				cosine = dot_product/(denom)
+			else:
+				cosine = 0
 			similarity.append(cosine)
 		self.similarities.append({"worker"+name: similarity})
 
-	def get_similarity_score(self, document, num_workers=4):
+	def get_similarity_score(self, document, top_k=5, num_workers=4):
 		data = document
 		data = data.lower()
 		data = re.sub("[^a-zA-Z]+", " ", data)
@@ -139,39 +145,41 @@ class TF_IDF_Vectorizer:
 				pass
 		worker_splits = []
 		last_idx = None
+		k = int(len(self.tf["rows"])/num_workers)
 		for worker in range(num_workers):
-			if worker!=num_workers-1:
-				rows_arr = self.tf["rows"][worker*int(len(self.tf["rows"])/num_workers):(worker+1)*int(len(self.tf["rows"])/num_workers)]
+			if worker==0:
+				rows_arr = self.tf["rows"][0:(worker+1)*k]
+			elif worker!=num_workers-1:
+				rows_arr = self.tf["rows"][worker*k-1:(worker+1)*k]
 			else:
-				rows_arr = self.tf["rows"][worker*int(len(self.tf["rows"])/num_workers):]
+				rows_arr = self.tf["rows"][worker*k-1:]
 			rows_arr = np.array(rows_arr)
-			if last_idx is None:
-				columns_arr = self.tf["columns"][rows_arr[0]:rows_arr[-1]]
-				values_arr = self.tf["value"][rows_arr[0]:rows_arr[-1]]
-				last_idx = rows_arr[-1]
-			else:
-				columns_arr = self.tf["columns"][last_idx:rows_arr[-1]]
-				values_arr = self.tf["value"][last_idx:rows_arr[-1]]
-				rows_arr = rows_arr - rows_arr[0]
+			columns_arr = self.tf["columns"][rows_arr[0]:rows_arr[-1]]
+			values_arr = self.tf["value"][rows_arr[0]:rows_arr[-1]]
 			tf = {"value": values_arr, "columns": columns_arr, "rows": rows_arr}
 			worker_splits.append(tf)
 
 		threads = []
 		for worker in range(num_workers):
 			threads.append(threading.Thread(target=self.get_document_similarity, args=(document_vec, worker_splits[worker],'t'+str(worker),), name='t'+str(worker)))
+		self.similarities = []
 		for worker in range(num_workers):
 			threads[worker].start()
 		for worker in range(num_workers):
 			threads[worker].join()
-		_ = [print(i.keys()) for i in self.similarities]
 		similarities = []
 		for worker in range(num_workers):
 			for items in self.similarities:
 				name = list(items.keys())[0]
 				if name=="workert"+str(worker):
 					similarities.extend(items[name])
-		print(len(similarities))
+		similarities = np.array(similarities)
+		ranking_indices = np.argsort(similarities)[::-1]
+		ranking = np.array(self.tf["files"])
+		ranking = ranking[ranking_indices]
+		return similarities[:top_k], ranking[:top_k]
 
 if __name__ == '__main__':
-	tf_idf = TF_IDF_Vectorizer()
-	tf_idf.get_similarity_score("002989 1968 2009 movie Color Norman, Matt (I) War History Drama Thriller vietnam-war black-panther-party assassination digit-in-title based-on-film robert-f-kennedy mexico sequel black-panthers year-in-title riot second-part olympics usa black-power number-in-title 1968 martin-luther-king-jr. English Australia 120 Norman, Matt (I) Norman, Rebecca (II) Favelle, Michael Pederson, Dave Norman, Matt (I) Norman, Matt (I) Based on a true story from the film \"SALUTE\". 1968 is the follow up feature drama to Matt Norman's International award winning documentary feature \"SALUTE\". This film reminds us what it was to live through 1968 in Mexico and The United States. Racism, Poverty, and War were all part of this incredible year. A Civil war began in the streets of the United States when Martin Luther King Jr was killed. The year was full of turmoil and what followed was the making of a year that will be held in history as one of the most violent and horrifying in history around the World. This is")
+	tf_idf = TF_IDF_Vectorizer(use_cached=True)
+	_, ranking = tf_idf.get_similarity_score("science fiction super hero movie")
+	print(ranking)
